c7f370b69d1579cf86fbb7d595b50f7489302d17
Muhammed Çayhan <99079608+Mcayhan@users.noreply.github.com>
2026-02-15 22:29:50 +0300

    Initial project upload

---
+++ src/Cisim.java
@@ -0,0 +1,33 @@
+package uko;
+
+/**
+ *
+ * @author Muhammed
+ */
+
+abstract class Cisim extends Object{
+    
+    private String isim;
+    int x;
+    int y;
+
+    public Cisim(String isim, int x, int y) {
+        this.isim = isim;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public int getXKonumu() {
+        return x;
+    }
+
+    public int getYKonumu() {
+        return y;
+    }
+
+    public abstract void bilgiGoster();
+}
---
+++ src/Kaynak.java
@@ -0,0 +1,28 @@
+package uko;
+
+/**
+ *
+ * @author Muhammed
+ */
+
+class Kaynak {
+    private String adi;
+    private int miktar;
+
+    public Kaynak(String adi, int miktar) {
+        this.adi = adi;
+        this.miktar = miktar;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public int getMiktar() {
+        return miktar;
+    }
+
+    public void setMiktar(int miktar) {
+        this.miktar = miktar;
+    }
+}
